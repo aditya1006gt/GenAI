226d314ce0c2570a258f8404491f436b4c1ed9fb
aditya1006gt <01aditya1006@gmail.com>
2026-05-16 09:37:16 +0530

    updated codes

---
+++ LLM.js
@@ -0,0 +1,17 @@
+// npm install @google/genai
+
+import "dotenv/config";
+import { GoogleGenAI } from "@google/genai";
+
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+main();
